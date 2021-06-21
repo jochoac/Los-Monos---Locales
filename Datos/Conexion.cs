a82eaf6535d77830161c4c161944460edd529b09
jochoac <32500353+jochoac@users.noreply.github.com>
2021-06-21 15:43:20 -0500

    Update Conexion.cs

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -11,9 +11,7 @@ namespace Datos
     {
         public SqlConnection conectar()
         {
-            //SqlConnection con = new SqlConnection("Persist Security Info=False;User ID=Monos;Password=Monos56++;Initial Catalog=Los_Monos;Server=192.168.0.32,1433");
-            //SqlConnection con = new SqlConnection("Server=tcp:losmonos.database.windows.net,1433;Initial Catalog=losmonos;Persist Security Info=False;User ID=monos;Password=Colombia2018*;MultipleActiveResultSets=False;TrustServerCertificate=False;Connection Timeout=1000;");
-            SqlConnection con = new SqlConnection("Data source=DESKTOP-USQ1V2V\\SQLEXPRESS;Initial Catalog= Los_Monos;integrated security= true");
+            SqlConnection con = new SqlConnection("Connection String goes here");
             return con;
         }
     }
